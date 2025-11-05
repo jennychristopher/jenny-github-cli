90a215054a84f17a0f045245ccae6c81fc56cb3f
Jenny <jenny.christopher@multicorewareinc.com>
2025-11-05 16:38:26 +0530

    Modify file structure

--- adddiv_model/tests/test_adddiv_model.py
+++ adddiv_model/tests/test_adddiv_model.py
@@ -1,6 +1,6 @@
 
 import math
-from adddiv_model import adddiv
+from adddiv_model.adddiv_model import adddiv
 
 def test_sigmoid_basic():
     assert math.isclose(adddiv(1,1,1), 2)
--- addmul_model/tests/test_addcmul.py
+++ addmul_model/tests/test_addcmul.py
@@ -1,5 +1,5 @@
 import math
-from adddiv_model import addmul
+from addmul_model.addmul_model import addmul
 
 def test_sigmoid_basic():
     assert math.isclose(addmul(1,1,1), 2)
