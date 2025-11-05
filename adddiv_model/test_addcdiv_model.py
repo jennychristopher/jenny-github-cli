823d1a5174510b9f0b116ff489dd67266edab315
Jenny <jenny.christopher@multicorewareinc.com>
2025-11-05 15:37:14 +0530

    Modify folder structure

--- adddiv_model/test_addcdiv_model.py
+++
@@ -1,5 +0,0 @@
-import math
-from adddiv_model import adddiv
-
-def test_addmul():
-    assert math.isclose(adddiv(1,1,1),2)
--- addmul_model/test_addcmul_model.py
+++
@@ -1,5 +0,0 @@
-import math
-from addmul_model import addmul
-
-def test_addmul():
-    assert math.isclose(addmul(1,1,1),2)
