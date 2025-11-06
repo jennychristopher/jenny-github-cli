3767e7b62bcf50b92765af15c89f99ba8c6ce62b
Jenny <jenny.christopher@multicorewareinc.com>
2025-11-06 15:09:07 +0530

    YAML MODIFIED

--- compound_model/tests/test_compound_model.py
+++ compound_model/tests/test_compound_model.py
@@ -1,6 +1,7 @@
 import math
 import pytest
 from compound_model.compound_model import compound_model
+from adddiv_model.adddiv_model import adddiv
 
 def test_compound_model():
     return math.isclose(compound_model(1,1,1), 4)
