36faf0c276badf6ed3f4a6230214eafee910008a
Jenny <jenny.christopher@multicorewareinc.com>
2025-11-07 14:04:44 +0530

    Include compound model testing inside addcdiv test

--- addcdiv_model/tests/test_addcdiv_model.py
+++ addcdiv_model/tests/test_addcdiv_model.py
@@ -1,5 +1,7 @@
 import math
 from addcdiv_model.addcdiv_model import addcdiv
+from compound_model.compound_model import compound_model
 
 def test_addcdiv():
     assert math.isclose(addcdiv(1,1,1), 2)
+    assert math.isclose(compound_model(1,1,1), 4)
