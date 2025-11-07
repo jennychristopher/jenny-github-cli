08df55357af30830692db766bd92d3ce15da7771
Jenny <jenny.christopher@multicorewareinc.com>
2025-11-07 10:59:31 +0530

    Include compound test inside addmul test

--- addmul_model/tests/test_addcmul.py
+++ addmul_model/tests/test_addcmul.py
@@ -1,8 +1,10 @@
 # Add Pytest for addmul custom model - new
 import math
 from addmul_model.addmul_model import addmul
+from compound_model.compound_model import compound_model
 import pytest
 
 
 def test_addmul():
     assert math.isclose(addmul(1,1,1), 2)
+    assert math.isclose(compound_model(1,1,1), 4)
