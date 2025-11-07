a010e4f7cdafd181cde83a915b0feaa6ad671490
Jenny <jenny.christopher@multicorewareinc.com>
2025-11-07 12:59:00 +0530

    Remove compound model

--- compound_model/compound_model.py
+++
@@ -1,7 +0,0 @@
-from addcmul_model.addcmul_model import addcmul
-from addcdiv_model.addcdiv_model import addcdiv
-
-def compound_model(a,b,c):
-    mul_part = addcmul(a,b,c)
-    div_part = addcdiv(a,b,c)
-    return mul_part + div_part
--- compound_model/tests/test_compound_model.py
+++
@@ -1,4 +0,0 @@
-import math
-from compound_model.compound_model import compound_model
-def test_compound_model():
-    assert math.isclose(compound_model(1,1,1), 4)
